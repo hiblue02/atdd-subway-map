83f926bf8415ac9477fabb3a66bbe98ecec247db
hiblue02 <38008976+hiblue02@users.noreply.github.com>
2023-06-27 02:01:15 +0900

    Revert "test: 구글 페이지 요청 구현 (#777)"
    This reverts commit 1cc00aeb356c4c40e96bfd357683474beb8e6675.

--- src/test/java/subway/RestAssuredTest.java
+++ src/test/java/subway/RestAssuredTest.java
@@ -1,6 +1,5 @@
 package subway;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -16,11 +15,8 @@ public class RestAssuredTest {
     @DisplayName("구글 페이지 접근 테스트")
     @Test
     void accessGoogle() {
-        ExtractableResponse<Response> response = RestAssured.when()
-                .get("https://google.com")
-                .then()
-                .statusCode(200)
-                .extract();
+        // TODO: 구글 페이지 요청 구현
+        ExtractableResponse<Response> response = null;
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
